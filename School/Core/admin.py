20fafc9d723c7268691e2805db7eb07112285d08
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2021-01-10 13:37:30 -0800

    updated admin.py added validation

--- School/Core/admin.py
+++ School/Core/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.db.models import Avg
 from django.utils.html import format_html
@@ -6,9 +7,29 @@ from django.utils.http import urlencode
 
 from .models import Person, Course, Grade
 
+class PersonAdminForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = "__all__"
+
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"] == "Spike":
+            raise forms.ValidationError("No Vampires!")
+
+        return self.cleaned_data["first_name"]
+
+
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "show_average")
+    search_fields = ("last_name__startswith", )
+    fields = ("first_name", "last_name", "courses")
+    form = PersonAdminForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["first_name"].label = "First Name (Humans Only!):"
+        return form
 
     def show_average(self, obj):
         result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
@@ -19,6 +40,7 @@ class PersonAdmin(admin.ModelAdmin):
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("name", "year", "view_students_link")
+    list_filter = ("year",)
 
     def view_students_link(self, obj):
         count = obj.person_set.count()
@@ -32,7 +54,7 @@ class CourseAdmin(admin.ModelAdmin):
 
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ("course__year",)
 
 
 
